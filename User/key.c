4b6d5eb4b4969b7cadfc57c91045c524ad5578a2
GodFox <zhangliuxian@gmail.com>
2013-07-02 15:11:13 +0800

    finish exti key driver to control Led

--- User/key.c
+++ User/key.c
@@ -2,6 +2,8 @@
 #include "usart.h"
 #include "stdio.h"
 
+unsigned char keyset = 0;
+
 void keyinit(void)
 {
 	
@@ -11,12 +13,13 @@ void keyinit(void)
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
 	
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_Init(GPIOC, &GPIO_InitStructure);
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_25MHz;
+  GPIO_Init(GPIOE, &GPIO_InitStructure);
 
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
   GPIO_Init(GPIOA, &GPIO_InitStructure);
@@ -39,30 +42,32 @@ void keyinit(void)
 void EXTI_Configuration(void)
 {
 	EXTI_InitTypeDef EXTI_InitStructure;
-	EXTI_InitStructure.EXTI_Line    = EXTI_Line0;
+	
+	EXTI_InitStructure.EXTI_Line    = EXTI_Line13;
 	EXTI_InitStructure.EXTI_Mode    = EXTI_Mode_Interrupt;
 	EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Falling;
 	EXTI_InitStructure.EXTI_LineCmd = ENABLE;
 	EXTI_Init(&EXTI_InitStructure);
 	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOC,GPIO_PinSource13);
 	
-	EXTI_InitStructure.EXTI_Line    = EXTI_Line1;
+	
+	EXTI_InitStructure.EXTI_Line    = EXTI_Line0;
 	EXTI_Init(&EXTI_InitStructure);
 	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA,GPIO_PinSource0);
 	
-	EXTI_InitStructure.EXTI_Line    = EXTI_Line2;
+	EXTI_InitStructure.EXTI_Line    = EXTI_Line1;
 	EXTI_Init(&EXTI_InitStructure);
 	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOE,GPIO_PinSource1);
 	
-	EXTI_InitStructure.EXTI_Line    = EXTI_Line3;
+	EXTI_InitStructure.EXTI_Line    = EXTI_Line4;
 	EXTI_Init(&EXTI_InitStructure);
 	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOE,GPIO_PinSource4);
 	
-	EXTI_InitStructure.EXTI_Line    = EXTI_Line4;
+	EXTI_InitStructure.EXTI_Line    = EXTI_Line5;
 	EXTI_Init(&EXTI_InitStructure);
 	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOE,GPIO_PinSource5);
 	
-	EXTI_InitStructure.EXTI_Line    = EXTI_Line5;
+	EXTI_InitStructure.EXTI_Line    = EXTI_Line6;
 	EXTI_Init(&EXTI_InitStructure);
 	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOE,GPIO_PinSource6);
 	
@@ -71,19 +76,18 @@ void EXTI_Configuration(void)
 void NVIC_Configuration(void)
 {
 	NVIC_InitTypeDef NVIC_InitStructure;
-	NVIC_InitStructure.NVIC_IRQChannel = EXTI0_IRQn;
+// 	NVIC_InitTypeDef NVIC_InitStructure1;
+	
+	NVIC_InitStructure.NVIC_IRQChannel = EXTI15_10_IRQn;
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
 	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
 	NVIC_Init(&NVIC_InitStructure);
 	
+	NVIC_InitStructure.NVIC_IRQChannel = EXTI0_IRQn;
+	NVIC_Init(&NVIC_InitStructure);
+	
 	NVIC_InitStructure.NVIC_IRQChannel = EXTI1_IRQn;
-	NVIC_Init(&NVIC_InitStructure);
-	
-	NVIC_InitStructure.NVIC_IRQChannel = EXTI2_IRQn;
-	NVIC_Init(&NVIC_InitStructure);
-	
-	NVIC_InitStructure.NVIC_IRQChannel = EXTI3_IRQn;
 	NVIC_Init(&NVIC_InitStructure);
 	
 	NVIC_InitStructure.NVIC_IRQChannel = EXTI4_IRQn;
@@ -94,12 +98,82 @@ void NVIC_Configuration(void)
 }
 
 
-void EXTI6_0_IRQHandler(void)
+void EXTI15_10_IRQHandler(void)
 {
-  printf("EXTI_0\n");
+	if (EXTI_GetITStatus(EXTI_Line13) != RESET)
+    {
+			EXTI_ClearITPendingBit(EXTI_Line13);	
+			if(!GPIO_ReadInputDataBit(GPIOC,GPIO_Pin_13))
+				{
+					//printf("key2\n");
+					GPIO_ResetBits(GPIOC,GPIO_Pin_13);
+					keyset = 2;
+				}
+		}
+}
+
+void EXTI0_IRQHandler(void)
+{
 	if (EXTI_GetITStatus(EXTI_Line0) != RESET)
     {
         EXTI_ClearITPendingBit(EXTI_Line0);
-				printf("key1\n");
+				if(!GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_0))
+				{
+					//printf("key1\n");
+					GPIO_ResetBits(GPIOA,GPIO_Pin_0);
+					keyset = 1;
+				}
+    }
+}
+
+void EXTI1_IRQHandler(void)
+{
+	if (EXTI_GetITStatus(EXTI_Line1) != RESET)
+    {
+        EXTI_ClearITPendingBit(EXTI_Line1);
+				if(!GPIO_ReadInputDataBit(GPIOE,GPIO_Pin_1))
+				{
+					//printf("key3\n");
+					GPIO_ResetBits(GPIOE,GPIO_Pin_1);
+					keyset = 3;
+				}
+    }
+}
+
+void EXTI4_IRQHandler(void)
+{
+	if (EXTI_GetITStatus(EXTI_Line4) != RESET)
+    {
+        EXTI_ClearITPendingBit(EXTI_Line4);
+				if(!GPIO_ReadInputDataBit(GPIOE,GPIO_Pin_4))
+				{
+					//printf("key4\n");
+					GPIO_ResetBits(GPIOE,GPIO_Pin_4);
+					keyset = 4;
+				}
+    }
+}
+
+void EXTI9_5_IRQHandler(void)
+{
+	if (EXTI_GetITStatus(EXTI_Line5) != RESET)
+    {
+        EXTI_ClearITPendingBit(EXTI_Line5);
+				if(!GPIO_ReadInputDataBit(GPIOE,GPIO_Pin_5))
+				{
+					//printf("key5\n");
+					GPIO_ResetBits(GPIOE,GPIO_Pin_5);
+					keyset = 5;
+				}
+    }
+	if (EXTI_GetITStatus(EXTI_Line6) != RESET)
+    {
+        EXTI_ClearITPendingBit(EXTI_Line6);
+				if(!GPIO_ReadInputDataBit(GPIOE,GPIO_Pin_6))
+				{
+					//printf("key6\n");
+					GPIO_ResetBits(GPIOE,GPIO_Pin_6);
+					keyset = 6;
+				}
     }
 }
--- User/main.c
+++ User/main.c
@@ -16,57 +16,60 @@ __task void LedTest(void)
 	printf("task LedTest\n");
 	while(1)
 	{
-		switch (LedCunt)
+		if(keyset == 0)
 		{
-			case 0:
+			switch (LedCunt)
 			{
-				GPIOD ->BSRRH |= GPIO_Pin_13;
-				GPIOD ->BSRRL |= GPIO_Pin_14;
-				GPIOD ->BSRRL |= GPIO_Pin_15;
-				GPIOC ->BSRRL |= GPIO_Pin_7;
-				//printf("Led 1 Light\n");
-				LedDelay();
-				LedCunt += 1;
-			}
-			case 1:
-			{
-				GPIOD ->BSRRH |= GPIO_Pin_13;
-				GPIOD ->BSRRH |= GPIO_Pin_14;
-				GPIOD ->BSRRL |= GPIO_Pin_15;
-				GPIOC ->BSRRL |= GPIO_Pin_7;
-				//printf("Led 1/2 Light\n");
-				LedDelay();
-				LedCunt += 1;
-			}
-			case 2:
-			{
-				GPIOD ->BSRRH |= GPIO_Pin_13;
-				GPIOD ->BSRRH |= GPIO_Pin_14;
-				GPIOD ->BSRRH |= GPIO_Pin_15;
-				GPIOC ->BSRRL |= GPIO_Pin_7;
-				//printf("Led 1/2/3 Light\n");
-				LedDelay();
-				LedCunt += 1;
-			}
-			case 3:
-			{
-				GPIOD ->BSRRH |= GPIO_Pin_13;
-				GPIOD ->BSRRH |= GPIO_Pin_14;
-				GPIOD ->BSRRH |= GPIO_Pin_15;
-				GPIOC ->BSRRH |= GPIO_Pin_7;
-				//printf("Led 1/2/3/4 Light\n");
-				LedDelay();
-				LedCunt += 1;
-			}
-			case 4:
-			{
-				GPIOD ->BSRRL |= GPIO_Pin_13;
-				GPIOD ->BSRRL |= GPIO_Pin_14;
-				GPIOD ->BSRRL |= GPIO_Pin_15;
-				GPIOC ->BSRRL |= GPIO_Pin_7;
-				//printf("No Led Light\n");
-				LedDelay();
-				LedCunt = 0;
+				case 0:
+				{
+					GPIOD ->BSRRH |= GPIO_Pin_13;
+					GPIOD ->BSRRL |= GPIO_Pin_14;
+					GPIOD ->BSRRL |= GPIO_Pin_15;
+					GPIOC ->BSRRL |= GPIO_Pin_7;
+					//printf("Led 1 Light\n");
+					LedDelay();
+					LedCunt += 1;
+				}
+				case 1:
+				{
+					GPIOD ->BSRRH |= GPIO_Pin_13;
+					GPIOD ->BSRRH |= GPIO_Pin_14;
+					GPIOD ->BSRRL |= GPIO_Pin_15;
+					GPIOC ->BSRRL |= GPIO_Pin_7;
+					//printf("Led 1/2 Light\n");
+					LedDelay();
+					LedCunt += 1;
+				}
+				case 2:
+				{
+					GPIOD ->BSRRH |= GPIO_Pin_13;
+					GPIOD ->BSRRH |= GPIO_Pin_14;
+					GPIOD ->BSRRH |= GPIO_Pin_15;
+					GPIOC ->BSRRL |= GPIO_Pin_7;
+					//printf("Led 1/2/3 Light\n");
+					LedDelay();
+					LedCunt += 1;
+				}
+				case 3:
+				{
+					GPIOD ->BSRRH |= GPIO_Pin_13;
+					GPIOD ->BSRRH |= GPIO_Pin_14;
+					GPIOD ->BSRRH |= GPIO_Pin_15;
+					GPIOC ->BSRRH |= GPIO_Pin_7;
+					//printf("Led 1/2/3/4 Light\n");
+					LedDelay();
+					LedCunt += 1;
+				}
+				case 4:
+				{
+					GPIOD ->BSRRL |= GPIO_Pin_13;
+					GPIOD ->BSRRL |= GPIO_Pin_14;
+					GPIOD ->BSRRL |= GPIO_Pin_15;
+					GPIOC ->BSRRL |= GPIO_Pin_7;
+					//printf("No Led Light\n");
+					LedDelay();
+					LedCunt = 0;
+				}
 			}
 		}
 	}
@@ -85,13 +88,187 @@ __task void Exti_key(void)
 {
 	
 	printf("task Exti Key\n");
-	while(1);
+	while(1)
+	{
+		if(keyset == 1)
+		{
+			switch (LedCunt)
+			{
+				case 0:
+				{
+					GPIOD ->BSRRH |= GPIO_Pin_13;
+					GPIOD ->BSRRL |= GPIO_Pin_14;
+					GPIOD ->BSRRL |= GPIO_Pin_15;
+					GPIOC ->BSRRL |= GPIO_Pin_7;
+					//printf("Led 1 Light\n");
+					LedDelay();
+					LedCunt += 1;
+				}
+				case 1:
+				{
+					GPIOD ->BSRRL |= GPIO_Pin_13;
+					GPIOD ->BSRRH |= GPIO_Pin_14;
+					GPIOD ->BSRRL |= GPIO_Pin_15;
+					GPIOC ->BSRRL |= GPIO_Pin_7;
+					//printf("Led 1/2 Light\n");
+					LedDelay();
+					LedCunt += 1;
+				}
+				case 2:
+				{
+					GPIOD ->BSRRL |= GPIO_Pin_13;
+					GPIOD ->BSRRL |= GPIO_Pin_14;
+					GPIOD ->BSRRH |= GPIO_Pin_15;
+					GPIOC ->BSRRL |= GPIO_Pin_7;
+					//printf("Led 1/2/3 Light\n");
+					LedDelay();
+					LedCunt += 1;
+				}
+				case 3:
+				{
+					GPIOD ->BSRRL |= GPIO_Pin_13;
+					GPIOD ->BSRRL |= GPIO_Pin_14;
+					GPIOD ->BSRRL |= GPIO_Pin_15;
+					GPIOC ->BSRRH |= GPIO_Pin_7;
+					//printf("Led 1/2/3/4 Light\n");
+					LedDelay();
+					LedCunt = 0;
+				}
+			}
+		}
+		else if (keyset == 2)
+		{
+			switch (LedCunt)
+			{
+				case 0:
+				{
+					GPIOD ->BSRRL |= GPIO_Pin_13;
+					GPIOD ->BSRRL |= GPIO_Pin_14;
+					GPIOD ->BSRRL |= GPIO_Pin_15;
+					GPIOC ->BSRRH |= GPIO_Pin_7;
+					//printf("Led 1 Light\n");
+					LedDelay();
+					LedCunt += 1;
+				}
+				case 1:
+				{
+					GPIOD ->BSRRL |= GPIO_Pin_13;
+					GPIOD ->BSRRL |= GPIO_Pin_14;
+					GPIOD ->BSRRH |= GPIO_Pin_15;
+					GPIOC ->BSRRL |= GPIO_Pin_7;
+					//printf("Led 1/2 Light\n");
+					LedDelay();
+					LedCunt += 1;
+				}
+				case 2:
+				{
+					GPIOD ->BSRRL |= GPIO_Pin_13;
+					GPIOD ->BSRRH |= GPIO_Pin_14;
+					GPIOD ->BSRRL |= GPIO_Pin_15;
+					GPIOC ->BSRRL |= GPIO_Pin_7;
+					//printf("Led 1/2/3 Light\n");
+					LedDelay();
+					LedCunt += 1;
+				}
+				case 3:
+				{
+					GPIOD ->BSRRH |= GPIO_Pin_13;
+					GPIOD ->BSRRL |= GPIO_Pin_14;
+					GPIOD ->BSRRL |= GPIO_Pin_15;
+					GPIOC ->BSRRL |= GPIO_Pin_7;
+					//printf("Led 1/2/3/4 Light\n");
+					LedDelay();
+					LedCunt = 0;
+				}
+			}
+		}
+		else if (keyset == 3)
+		{
+			switch (LedCunt)
+			{
+				case 0:
+				{
+					GPIOD ->BSRRH |= GPIO_Pin_13;
+					GPIOD ->BSRRH |= GPIO_Pin_14;
+					GPIOD ->BSRRH |= GPIO_Pin_15;
+					GPIOC ->BSRRH |= GPIO_Pin_7;
+					//printf("Led 1 Light\n");
+					LedDelay();
+					LedCunt += 1;
+				}
+				case 1:
+				{
+					GPIOD ->BSRRL |= GPIO_Pin_13;
+					GPIOD ->BSRRL |= GPIO_Pin_14;
+					GPIOD ->BSRRL |= GPIO_Pin_15;
+					GPIOC ->BSRRL |= GPIO_Pin_7;
+					//printf("Led 1/2 Light\n");
+					LedDelay();
+					LedCunt = 0;
+				}
+			}
+		}
+		else if(keyset == 4)
+		{
+			switch (LedCunt)
+			{
+				case 0:
+				{
+					GPIOD ->BSRRH |= GPIO_Pin_13;
+					GPIOD ->BSRRL |= GPIO_Pin_14;
+					GPIOD ->BSRRL |= GPIO_Pin_15;
+					GPIOC ->BSRRH |= GPIO_Pin_7;
+					//printf("Led 1 Light\n");
+					LedDelay();
+					LedCunt += 1;
+				}
+				case 1:
+				{
+					GPIOD ->BSRRL |= GPIO_Pin_13;
+					GPIOD ->BSRRH |= GPIO_Pin_14;
+					GPIOD ->BSRRH |= GPIO_Pin_15;
+					GPIOC ->BSRRL |= GPIO_Pin_7;
+					//printf("Led 1/2 Light\n");
+					LedDelay();
+					LedCunt = 0;
+				}
+			}
+		}
+		else if(keyset == 5)
+		{
+			switch (LedCunt)
+			{
+				case 0:
+				{
+					GPIOD ->BSRRH |= GPIO_Pin_13;
+					GPIOD ->BSRRL |= GPIO_Pin_14;
+					GPIOD ->BSRRH |= GPIO_Pin_15;
+					GPIOC ->BSRRL |= GPIO_Pin_7;
+					//printf("Led 1 Light\n");
+					LedDelay();
+					LedCunt += 1;
+				}
+				case 1:
+				{
+					GPIOD ->BSRRL |= GPIO_Pin_13;
+					GPIOD ->BSRRH |= GPIO_Pin_14;
+					GPIOD ->BSRRL |= GPIO_Pin_15;
+					GPIOC ->BSRRH |= GPIO_Pin_7;
+					//printf("Led 1/2 Light\n");
+					LedDelay();
+					LedCunt = 0;
+				}
+			}
+		}
+		else if (keyset == 6)
+			keyset = 0;
+	}
 }
 
 __task void run(void)
 {
 	tsk_1 = os_tsk_create(LedTest,0);
-	//tsk_2 = os_tsk_create(Inter_Uart,0);
+	tsk_2 = os_tsk_create(Inter_Uart,0);
 	tsk_3 = os_tsk_create(Exti_key,0);
 	
 	os_tsk_delete_self();
